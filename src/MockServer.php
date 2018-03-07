a2a76ec97b72294cee7dfd52d322ea641fe8e514
Tadas <tadas@edmondscommerce.co.uk>
2018-03-07 17:32:59 +0000

    Added root user to pgrep command

--- src/MockServer.php
+++ src/MockServer.php
@@ -174,7 +174,7 @@ class MockServer
     {
         //-x Preg matches only on exact names instead of partial match
         //-f Matches against the process name AND the arguments for us to denote the web server from other PHP processes
-        $command = 'pgrep -u "$(whoami)" -f "php -S"';
+        $command = 'pgrep -u "$(whoami),root" -f "php -S"';
 
         exec($command, $commandOutput, $exitCode);
 
