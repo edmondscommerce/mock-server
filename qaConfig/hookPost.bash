18d0b049672b37b9630a937755b090a9541c4803
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-07-11 10:02:11 +0000

    removing some cruft

--- qaConfig/hookPost.bash
+++ qaConfig/hookPost.bash
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
-cd $DIR;
-set -e
-set -u
-set -o pipefail
-standardIFS="$IFS"
-IFS=$'\n\t'
 echo "
 ===========================================
 $(hostname) $0 $@
