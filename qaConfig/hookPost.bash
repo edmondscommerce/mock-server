03129db7b8afa739881f2ede348d65cd903a6b11
Joseph Edmonds <joseph@edmondscommerce.co.uk>
2018-07-11 09:46:14 +0000

    clearing up any dangling child processes (presumbably cuased by infection mutants not killing off their child processes)

---
+++ qaConfig/hookPost.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+cd $DIR;
+set -e
+set -u
+set -o pipefail
+standardIFS="$IFS"
+IFS=$'\n\t'
+echo "
+===========================================
+$(hostname) $0 $@
+===========================================
+"
+
+echo "Ensuring any child processes are killed"
+[[ -z "$(jobs -p)" ]] || kill $(jobs -p)
+
+echo "
+===========================================
+$(hostname) $0 $@ COMPLETED
+===========================================
+"
